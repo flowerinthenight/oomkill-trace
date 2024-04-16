22c80c376c12705fcecb10702651086c09eb1f88
flowerinthenight <root@flowerinthenight.com>
2024-04-16 20:29:00 +0900

    test subrun

--- main.go
+++ main.go
@@ -1,7 +1,84 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"os/signal"
+	"sync"
+	"syscall"
+)
 
 func main() {
-	log.Println("hello")
+	// cmd := exec.Command("oomkill-bpfcc")
+	cmd := exec.Command("execsnoop-bpfcc") // test
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	outpipe, err := cmd.StdoutPipe()
+	if err != nil {
+		slog.Error("StdoutPipe failed:", "err", err)
+		return
+	}
+
+	errpipe, err := cmd.StderrPipe()
+	if err != nil {
+		slog.Error("StdoutPipe failed:", "err", err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	err = cmd.Start()
+	if err != nil {
+		slog.Error("Start failed:", "err", err)
+		return
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		s := make(chan os.Signal)
+		signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
+		sig := fmt.Sprintf("%s", <-s)
+		slog.Info("terminated, cleaning up...", "sig", sig)
+
+		err := cmd.Process.Signal(syscall.SIGTERM)
+		if err != nil {
+			slog.Info("failed to terminate process, force kill...")
+			cmd.Process.Signal(syscall.SIGKILL)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		outscan := bufio.NewScanner(outpipe)
+		for {
+			chk := outscan.Scan()
+			if !chk {
+				break
+			}
+
+			stxt := outscan.Text()
+			slog.Info("stdout:", "line", stxt)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		errscan := bufio.NewScanner(errpipe)
+		for {
+			chk := errscan.Scan()
+			if !chk {
+				break
+			}
+
+			stxt := errscan.Text()
+			slog.Info("stderr:", "line", stxt)
+		}
+	}()
+
+	cmd.Wait()
+	wg.Wait()
 }
